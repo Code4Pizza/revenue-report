35638e90bb50f0fe40b801ff913c9566c0b992b5
TheAnh <theanhvo0903@gmail.com>
2018-05-24 18:51:44 +0700

    init

---
+++ app/src/main/java/com/fr/fbsreport/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.fr.fbsreport
+
+import android.os.Bundle
+import com.fr.fbsreport.base.BaseActivity
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
---
+++ app/src/main/java/com/fr/fbsreport/base/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.fr.fbsreport.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
